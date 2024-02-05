498cb4fea067cbf63dab20303973db09d35b76a7
TowzinNegar <TowzinNegar@TOWZINNEGAR>
2024-02-05 15:15:47 +0330

    Add project files.

---
+++ Core8_BS5_PersianDatePicker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Core8_BS5_PersianDatePicker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
